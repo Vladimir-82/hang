7b4750b5dbc2413aaca38d198357fd8dc5997030
vladimir <grun_gespenst@tut.by>
2020-12-13 17:25:27 +0300

    что-то делать с пересечением и вводом данных в функцию

--- deviser.py
+++ deviser.py
@@ -4,25 +4,15 @@ def deviser(*args):
     for arg in args:
         lists.append(set([i for i in range(2, arg + 1) if arg % i == 0]))
     print(lists)
-    print(max(set.intersection(lists[0], lists[1], lists[2])))
-num1=30
-num2=20
-num3=10
-deviser(num1, num2, num3)
 
+    inters=set.intersection(lists[0], lists[1])#нужно что-то делать
+    print(inters)
+    print(max(inters))
 
+count_of_numbers=int(input('Введите колличество чисел:'))#нужно что-то делать
+for i in range(count_of_numbers):
+    num_list=[]
+    num=int(input('Введите число:'))
+    num_list.append(num)
 
-'''
-def deviser(num_1, num_2):
-    x1_numbers = [i for i in range(2, num_1+1) if num1 % i == 0]
-    x2_numbers = [i for i in range(2, num_2+1) if num2 % i == 0]
-
-    if len(set.intersection(set(x1_numbers), set(x2_numbers))) >= 1:
-        print(max(set(x1_numbers) & set(x2_numbers)))
-    else:
-        print('No divisers')
-
-num1=30
-num2=3
-deviser(num1, num2)
-'''
+deviser(10, 20)
