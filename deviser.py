808666f4fe3e8a4586d4e7d7cf1071bcc7f43bf8
vladimir <grun_gespenst@tut.by>
2020-12-13 20:46:59 +0300

    Все работает!

--- deviser.py
+++ deviser.py
@@ -1,18 +1,30 @@
-
-def deviser(*args):
+'''Программа ищет общий делитель у заданного колличества чисел'''
+def deviser(num_list):
     lists=[]
-    for arg in args:
-        lists.append(set([i for i in range(2, arg + 1) if arg % i == 0]))
-    print(lists)
+    all_numbers=[]
+    inter=[]
+    for arg in num_list:
+        lists.append([i for i in range(2, arg + 1) if arg % i == 0])
 
-    inters=set.intersection(lists[0], lists[1])#нужно что-то делать
-    print(inters)
-    print(max(inters))
+    for i in lists:
+        for j in i:
+            all_numbers.append(j)
+    for i in all_numbers:
+        if all_numbers.count(i)==len(lists):
+            inter.append(i)
 
-count_of_numbers=int(input('Введите колличество чисел:'))#нужно что-то делать
+    if len(inter)>=1:
+        print(f'Общий делитель у чисел: {max(inter)}')
+    else:
+        print('У этих чисел нет общего делителя!')
+
+count_of_numbers=1
+while count_of_numbers == 1 or count_of_numbers == 0:
+    count_of_numbers=int(input('Введите колличество чисел равное 2 или более:'))
+
+num_list=[]
 for i in range(count_of_numbers):
-    num_list=[]
     num=int(input('Введите число:'))
     num_list.append(num)
 
-deviser(10, 20)
+deviser(num_list)
